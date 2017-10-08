0265ab6c096392fe2d73b09f41bf29972df8ca22
nolanpster <nolanpster@gmail.com>
2017-10-08 10:13:19 -0400

    Updated code formatting in MDP.

--- MDP.py
+++ MDP.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
 __author__ = 'Jie Fu, jfu2@wpi.edu'
-from NFA import *
+from NFA_DFA_Module.NFA import NFA
+
 from scipy import stats
 import numpy as np
+
 
 class MDP:
     """A Markov Decision Process, defined by an initial state,
@@ -18,7 +21,8 @@ class MDP:
         tuple.  AP: a set of atomic propositions. Each proposition is
         identified by an index between 0 -N.  L: the labeling
         function, implemented as a dictionary: state: a subset of AP."""
-    def __init__(self, init=None, actlist=[], states=[], prob=dict([]), acc= None, gamma=.9, AP=set([]), L=dict([])):
+    def __init__(self, init=None, actlist=[], states=[], prob=dict([]),
+                 acc= None, gamma=.9, AP=set([]), L=dict([])):
         self.init=init
         self.actlist=actlist
         self.states=states
@@ -28,25 +32,29 @@ class MDP:
         self.prob=prob
         self.AP=AP
         self.L=L
-            
+
     def R(self, state):
         "Return a numeric reward for this state."
         return self.reward[state]
-    
-    def T(self,state, action):
-        """Transition model.  From a state and an action, return a row in the matrix for next-state probability."""
+
+    def T(self, state, action):
+        """
+        Transition model.  From a state and an action, return a row in the
+        matrix for next-state probability.
+        """
         i=self.states.index(state)
-        return self.prob[action][i,:] 
+        return self.prob[action][i, :]
 
-
-
-    def P(self,state, action, next_state):
-        "Derived from the transition model. For a state, an action and the next_state, return the probability of this transition."
+    def P(self, state, action, next_state):
+        """
+        Derived from the transition model. For a state, an action and the
+        next_state, return the probability of this transition.
+        """
         i=self.states.index(state)
         j=self.states.index(next_state)
-        return self.prob[action][i,j]
+        return self.prob[action][i, j]
 
-    def actions(self,state):
+    def actions(self, state):
         N=len(self.states)
         S=set([])
         for a in self.actlist:
@@ -54,137 +62,145 @@ class MDP:
                 S.add(a)
         return S
 
-    def labeling(self,s,A):
+    def labeling(self, s, A):
         self.L[s]=A
-    
-    def sample(self, state,action, num=1):
-        """Sample the next state according to the current state, the action, and the transition probability. """
+
+    def sample(self, state, action, num=1):
+        """
+        Sample the next state according to the current state, the action, and
+        the transition probability.
+        """
         if action not in self.actions(state):
             return None # Todo: considering adding the sink state
         N=len(self.states)
         i=self.states.index(state)
-        next_index= np.random.choice(N,num, p=self.prob[action][i,:])[0] # Note that only one element is chosen from the array, which is the output by random.choice
+        # Note that only one element is chosen from the array, which is the
+        # output by random.choice
+        next_index= np.random.choice(N, num, p=self.prob[action][i,:])[0]
         return self.states[next_index]
 
-    
-def productMDP(mdp,dra):
-    pmdp=MDP()
-    init=(mdp.init, dra.get_transition( mdp.L[mdp.init],dra.initial_state))
-    states=[]
-    for s in mdp.states:
-        for q in dra.states:
-            states.append((s,q))
-    N=len(states)
-    pmdp.init=init
-    pmdp.actlist=list(mdp.actlist)
-    pmdp.states=list(states)
-    for a in pmdp.actlist:
-        pmdp.prob[a]=np.zeros((N,N))
-        for i in range(N):
-            (s,q)=pmdp.states[i]
-        
-            pmdp.L[(s,q)]=mdp.L[s]
-            for j in range(N):
-                (next_s,next_q)=pmdp.states[j]
-                if next_q == dra.get_transition(mdp.L[next_s],q):
-                    p=mdp.P(s,a,next_s)
-                    pmdp.prob[a][i,j]= p
-    mdp_acc=[]
-    for (J,K) in dra.acc:
-        Jmdp=set([])
-        Kmdp=set([])
-        for s in states:
-            if s[1] in J:
-                Jmdp.add(s)
-            if s[1] in K:
-                Kmdp.add(s)
-        mdp_acc.append((Jmdp,Kmdp))
-    pmdp.acc=mdp_acc
-    return pmdp
-                        
-
-
-def get_NFA(mdp):
-    """
-    This function obtains the graph structure, which is essentially an non-deterministic finite state automaton from the original mdp.
-    """
-    nfa=NFA()
-    nfa.initial_state=mdp.init
-    nfa.states=mdp.states
-    nfa.alphabet=mdp.actlist
-    for a in mdp.actlist:
+    @staticmethod
+    def productMDP(mdp, dra):
+        pmdp=MDP()
+        init=(mdp.init, dra.get_transition(mdp.L[mdp.init], dra.initial_state))
+        states=[]
         for s in mdp.states:
-            next_state_list=[]
-            for next_s in mdp.states:
-                if mdp.prob[a][mdp.states.index(s), mdp.states.index(next_s)] != 0:
-                    next_state_list.append(next_s)
-            nfa.add_transition(a,s, next_state_list)
-    nfa.final_states=mdp.terminals
-    return nfa
+            for q in dra.states:
+                states.append((s, q))
+        N=len(states)
+        pmdp.init=init
+        pmdp.actlist=list(mdp.actlist)
+        pmdp.states=list(states)
+        for a in pmdp.actlist:
+            pmdp.prob[a]=np.zeros((N, N))
+            for i in range(N):
+                (s,q)=pmdp.states[i]
 
-def sub_MDP(mdp,H):
-    """
-    For a given MDP and a subset of the states H, construct a sub-mdp
-    that only includes the set of states in H, and a sink states for
-    all transitions to and from a state outside H.
-    """
-    if H == set(mdp.states): # If H is the set of states in mdp, return mdp as it is.
+                pmdp.L[(s,q)]=mdp.L[s]
+                for j in range(N):
+                    (next_s,next_q)=pmdp.states[j]
+                    if next_q == dra.get_transition(mdp.L[next_s], q):
+                        p=mdp.P(s,a,next_s)
+                        pmdp.prob[a][i, j]= p
+        mdp_acc=[]
+        for (J,K) in dra.acc:
+            Jmdp=set([])
+            Kmdp=set([])
+            for s in states:
+                if s[1] in J:
+                    Jmdp.add(s)
+                if s[1] in K:
+                    Kmdp.add(s)
+            mdp_acc.append((Jmdp, Kmdp))
+        pmdp.acc=mdp_acc
+        return pmdp
+
+
+    @staticmethod
+    def get_NFA(mdp):
+        """
+        This function obtains the graph structure, which is essentially an
+        non-deterministic finite state automaton from the original mdp.
+        """
+        nfa=NFA()
+        nfa.initial_state=mdp.init
+        nfa.states=mdp.states
+        nfa.alphabet=mdp.actlist
+        for a in mdp.actlist:
+            for s in mdp.states:
+                next_state_list=[]
+                for next_s in mdp.states:
+                    if mdp.prob[a][mdp.states.index(s), mdp.states.index(next_s)] != 0:
+                        next_state_list.append(next_s)
+                nfa.add_transition(a, s, next_state_list)
+        nfa.final_states=mdp.terminals
+        return nfa
+
+    @staticmethod
+    def sub_MDP(mdp, H):
+        """
+        For a given MDP and a subset of the states H, construct a sub-mdp
+        that only includes the set of states in H, and a sink states for
+        all transitions to and from a state outside H.
+        """
+        if H == set(mdp.states):
+            # If H is the set of states in mdp, return mdp as it is.
+            return mdp
+        submdp=MDP()
+        submdp.states=list(H)
+        submdp.states.append(-1) # -1 is the sink state.
+        N=len(submdp.states)
+        submdp.actlist=list(mdp.actlist)
+        submdp.prob={a:np.zeros((N, N)) for a in submdp.actlist}
+        temp=np.zeros(len(mdp.states))
+        for k in set(mdp.states) - H:
+            temp[mdp.states.index(k)]=1
+        for a in submdp.actlist:
+            for s in H: # except the last sink state.
+                i=submdp.states.index(s)
+                for next_s in H:
+                    j=submdp.states.index(next_s)
+                    submdp.prob[a][i, j] = mdp.P(s, a, next_s)
+                submdp.prob[a][i, -1]= np.inner(mdp.T(s, a), temp)
+            submdp.prob[a][submdp.states.index(-1), submdp.states.index(-1)]=1
+        acc=[]
+        for (J,K) in mdp.acc:
+            Jsub = set(H).intersection(J)
+            Ksub = set(H).intersection(K)
+            acc.append((Jsub,Ksub))
+        acc.append(({}, {-1}))
+        submdp.acc = acc
+        return submdp
+
+
+    @staticmethod
+    def read_from_file_MDP(fname):
+        """
+        This function takes the input file and construct an MDP based on thei
+        transition relations. The first line of the file is the list of states.
+        The second line of the file is the list of actions.
+        Starting from the second line, we have
+        state, action, next_state, probability
+        """
+        f=open(fname, 'r')
+        array = []
+        for line in f:
+            array.append( line.strip('\n') )
+        f.close()
+        mdp=MDP()
+        state_str=array[0].split(",")
+        mdp.states=[int(i) for i in state_str]
+        act_str=array[1].split(",")
+        mdp.actlist=act_str
+        mdp.prob=dict([])
+        N=len(mdp.states)
+        for a in mdp.actlist:
+            mdp.prob[a]=np.zeros((N, N))
+        for line in array[2: len(array)]:
+            trans_str=line.split(",")
+            state=int(trans_str[0])
+            act=trans_str[1]
+            next_state=int(trans_str[2])
+            p=float(trans_str[3])
+            mdp.prob[act][mdp.states.index(state), mdp.states.index(next_state)]=p
         return mdp
-    submdp=MDP()
-    submdp.states=list(H)
-    submdp.states.append(-1) # -1 is the sink state.
-    N=len(submdp.states)
-    submdp.actlist=list(mdp.actlist)
-    submdp.prob={a:np.zeros((N,N)) for a in submdp.actlist}
-    temp=np.zeros(len(mdp.states))
-    for k in set(mdp.states) - H:
-        temp[mdp.states.index(k)]=1
-    for a in submdp.actlist:
-        for s in H: # except the last sink state.
-            i=submdp.states.index(s)
-            for next_s in H:
-                j=submdp.states.index(next_s)
-                submdp.prob[a][i,j] = mdp.P(s,a,next_s)
-            submdp.prob[a][i,-1]= np.inner(mdp.T(s,a), temp)
-        submdp.prob[a][submdp.states.index(-1), submdp.states.index(-1)]=1
-    acc=[]
-    for (J,K) in mdp.acc:
-        Jsub = set(H).intersection(J)
-        Ksub = set(H).intersection(K)
-        acc.append((Jsub,Ksub))
-    acc.append(({},{-1}))
-    submdp.acc = acc
-    return submdp
-
-
-def read_from_file_MDP(fname):
-    """
-    This function takes the input file and construct an MDP based on the transition relations.
-    The first line of the file is the list of states.
-    The second line of the file is the list of actions.
-    Starting from the second line, we have
-    state, action, next_state, probability
-    """
-    f=open(fname,'r')
-    array = []
-    for line in f:
-        array.append( line.strip('\n') )
-    f.close() 
-    mdp=MDP()
-    state_str=array[0].split(",")
-    mdp.states=[int(i) for i in state_str]
-    act_str=array[1].split(",")
-    mdp.actlist=act_str
-    mdp.prob=dict([])
-    N=len(mdp.states)
-    for a in mdp.actlist:
-        mdp.prob[a]=np.zeros((N,N))
-    for line in array[2: len(array)]:
-        trans_str=line.split(",")
-        state=int(trans_str[0])
-        act=trans_str[1]
-        next_state=int(trans_str[2])
-        p=float(trans_str[3])
-        mdp.prob[act][mdp.states.index(state),mdp.states.index(next_state)]=p
-    return mdp
-
