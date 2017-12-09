36f14d521998d90f7743a8ecdf1760bdd1870112
Nolan Poulin <nolanpster@gmail.com>
2017-12-09 16:51:30 +0000

    Adapted A* to run in this projects map-format.

--- MDP_EM/MDP_EM/astar.py
+++ MDP_EM/MDP_EM/astar.py
@@ -22,6 +22,9 @@ from heapq import heappush, heappop # for priority queue
 import math
 import time
 import random
+import numpy as np
+from itertools import product
+
 
 class node:
     xPos = 0 # x position
@@ -35,14 +38,17 @@ class node:
         self.priority = priority
     def __lt__(self, other): # comparison method for priority queue
         return self.priority < other.priority
+
     def updatePriority(self, xDest, yDest):
         self.priority = self.distance + self.estimate(xDest, yDest) * 10 # A*
+
     # give higher priority to going straight instead of diagonally
     def nextMove(self, dirs, d): # d: direction to move
         if dirs == 8 and d % 2 != 0:
             self.distance += 14
         else:
             self.distance += 10
+
     # Estimation function for the remaining distance to the goal.
     def estimate(self, xDest, yDest):
         xd = xDest - self.xPos
@@ -135,81 +141,69 @@ def pathFind(the_map, n, m, dirs, dx, dy, xA, yA, xB, yB):
                         pqi = 1 - pqi
                     while len(pq[pqi]) > 0:
                         heappush(pq[1-pqi], pq[pqi][0])
-                        heappop(pq[pqi])       
+                        heappop(pq[pqi])
                     pqi = 1 - pqi
                     heappush(pq[pqi], m0) # add the better node instead
     return '' # if no route found
 
 # MAIN
-dirs = 8 # number of possible directions to move on the map
-if dirs == 4:
-    dx = [1, 0, -1, 0]
-    dy = [0, 1, 0, -1]
-elif dirs == 8:
-    dx = [1, 1, 0, -1, -1, -1, 0, 1]
-    dy = [0, 1, 1, 1, 0, -1, -1, -1]
+if __name__=='__main__':
+    dirs = 4 # number of possible directions to move on the map
+    if dirs == 4:
+        # [East, South, West, North]
+        acts = ['East', 'South', 'West', 'North']
+        dx = [1, 0, -1, 0]
+        dy = [0, 1, 0, -1]
+    elif dirs == 8:
+        dx = [1, 1, 0, -1, -1, -1, 0, 1]
+        dy = [0, 1, 1, 1, 0, -1, -1, -1]
 
-n = 30 # horizontal size of the map
-m = 30 # vertical size of the map
-the_map = []
-row = [0] * n
-for i in range(m): # create empty map
-    the_map.append(list(row))
+    n = 3 # horizontal size of the map
+    m = 2 # vertical size of the map
+    the_map = np.zeros([2,3])
 
-# fillout the map with a '+' pattern
-for x in range(n / 8, n * 7 / 8):
-    the_map[m / 2][x] = 1
-for y in range(m/8, m * 7 / 8):
-    the_map[y][n / 2] = 1
+    for xA, yA, in product(range(the_map.shape[0]),range(the_map.shape[1])):
+        for xB, yB in product(range(the_map.shape[0]),range(the_map.shape[1])):
+            the_map = np.zeros([2,3])
+            print 'Map size (X,Y): ', n, m
+            print 'Start: ', xA, yA
+            print 'Finish: ', xB, yB
+            t = time.time()
+            route = pathFind(the_map, n, m, dirs, dx, dy, xA, yA, xB, yB)
+            print 'Time to generate the route (seconds): ', time.time() - t
+            print 'Route:'
+            print route
+            print 'Actions'
+            for idx in route:
+                print acts[int(idx)]
 
-# randomly select start and finish locations from a list
-sf = []
-sf.append((0, 0, n - 1, m - 1))
-sf.append((0, m - 1, n - 1, 0))
-sf.append((n / 2 - 1, m / 2 - 1, n / 2 + 1, m / 2 + 1))
-sf.append((n / 2 - 1, m / 2 + 1, n / 2 + 1, m / 2 - 1))
-sf.append((n / 2 - 1, 0, n / 2 + 1, m - 1))
-sf.append((n / 2 + 1, m - 1, n / 2 - 1, 0))
-sf.append((0, m / 2 - 1, n - 1, m / 2 + 1))
-sf.append((n - 1, m / 2 + 1, 0, m / 2 - 1))
-(xA, yA, xB, yB) = random.choice(sf)
+            # mark the route on the map
+            if len(route) > 0:
+                x = xA
+                y = yA
+                the_map[y][x] = 2
+                for i in range(len(route)):
+                    j = int(route[i])
+                    x += dx[j]
+                    y += dy[j]
+                    the_map[y][x] = 3
+                the_map[y][x] = 4
 
-print 'Map size (X,Y): ', n, m
-print 'Start: ', xA, yA
-print 'Finish: ', xB, yB
-t = time.time()
-route = pathFind(the_map, n, m, dirs, dx, dy, xA, yA, xB, yB)
-print 'Time to generate the route (seconds): ', time.time() - t
-print 'Route:'
-print route
+            # display the map with the route added
+            print 'Map:'
+            for y in range(m):
+                for x in range(n):
+                    xy = the_map[y][x]
+                    if xy == 0:
+                        print '.', # space
+                    elif xy == 1:
+                        print 'O', # obstacle
+                    elif xy == 2:
+                        print 'S', # start
+                    elif xy == 3:
+                        print 'R', # route
+                    elif xy == 4:
+                        print 'F', # finish
+                print
 
-# mark the route on the map
-if len(route) > 0:
-    x = xA
-    y = yA
-    the_map[y][x] = 2
-    for i in range(len(route)):
-        j = int(route[i])
-        x += dx[j]
-        y += dy[j]
-        the_map[y][x] = 3
-    the_map[y][x] = 4
-
-# display the map with the route added
-print 'Map:'
-for y in range(m):
-    for x in range(n):
-        xy = the_map[y][x]
-        if xy == 0:
-            print '.', # space
-        elif xy == 1:
-            print 'O', # obstacle
-        elif xy == 2:
-            print 'S', # start
-        elif xy == 3:
-            print 'R', # route
-        elif xy == 4:
-            print 'F', # finish
-    print
-
-raw_input('Press Enter...')
+    raw_input('Press Enter...')
