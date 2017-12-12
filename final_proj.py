603876bb2f70022379fb58eb677e9642db9762a0
Nolan Poulin <nolanpster@gmail.com>
2017-12-12 16:45:55 -0500

    Using 4x4, inference works ok, can save policies as csv.

--- final_proj.py
+++ final_proj.py
@@ -9,8 +9,10 @@ from MDP_EM.MDP_EM.grid_graph import GridGraph
 
 import os
 import datetime
+import time
 import pickle
 import dill # For pickling lambda functions.
+import csv
 import numpy as np
 from numpy import ma
 from copy import deepcopy
@@ -160,7 +162,7 @@ infer_act_prob = {'North': np.array([[0.0, 1.0, 0.0, 0.0, 0.0],
                                     )
                  }
 
-grid_dim = [5,5] # [num-rows, num-cols]
+grid_dim = [4,4] # [num-rows, num-cols]
 grid_map = np.array(range(0,np.prod(grid_dim)), dtype=np.int8).reshape(grid_dim)
 states = [str(state) for state in range(grid_map.size)]
 
@@ -174,9 +176,9 @@ atom_prop = [green, red, empty]
 action_list = ['Empty', 'North', 'South', 'East', 'West']
 initial_state = '0'
 labels = {state: empty for state in states}
-labels['12'] = red
-labels['13'] = red
-labels['14'] = red
+labels['6'] = red
+labels['7'] = red
+labels['8'] = red
 labels['0'] = green
 
 def makeGridMDPxDRA(do_print=False):
@@ -278,12 +280,12 @@ def makeGridMDPxDRA(do_print=False):
             EM_policy[state][act] = EM_prob
             policy_difference[state][act] = round(abs(VI_prob - EM_prob),
                                                   compare_to_decimals)
-
     if do_print:
         print("Policy Difference:")
         pprint(policy_difference)
     # Solved mdp.
-    return EM_game_mdp, VI_game_mdp, policy_keys_to_print
+    return EM_game_mdp, VI_game_mdp, policy_keys_to_print, policy_difference
+
 
 class PlotGrid(object):
     def __init__(self, maze_cells, cmap):
@@ -293,12 +295,22 @@ class PlotGrid(object):
         self.maze_cells = maze_cells
         self.grid_dim = maze_cells.shape
         self.x, self.y = np.meshgrid(np.arange(grid_dim[1]+1), np.arange(grid_dim[0]+1))
+        self.cmap = cmap
 
     def configurePlot(self, title):
         fig, ax = plt.subplots()
         self.quadmesh = ax.pcolormesh(self.x, self.y, self.maze_cells, edgecolor='k', cmap=self.cmap)
         plt.title(title)
         return fig, ax
+
+
+class PlotKernel(PlotGrid):
+    def __init__(self, maze_cells, cmap):
+        super(self.__class__, self).__init__(maze_cells, cmap)
+
+    def configurePlot(self, title, cell, action, kernels):
+        fig, ax = super(self.__class__, self).configurePlot(title)
+        this_kern_act_phi = np.array([kernels[cell](state, act) for state in range(grid_map.size)]).reshape(grid_dim)
 
 
 class PlotPolicy(PlotGrid):
@@ -308,7 +320,6 @@ class PlotPolicy(PlotGrid):
         self.x_cent = self.x[:-1,:-1].ravel()+center_offset
         self.y_cent = self.y[:-1,:-1].ravel()+center_offset
         self.zero_mag = np.zeros(np.array(self.grid_dim))
-        self.cmap = cmap
         # Make these configurable eventually.
         self.quiv_angs = {'North': np.pi/2, 'South': -np.pi/2, 'East': 0, 'West': np.pi}
         self.quiv_scale = 25
@@ -359,27 +370,52 @@ class PlotPolicy(PlotGrid):
         return fig
 
 
-
-
 # Entry point when called from Command line.
 if __name__=='__main__':
     # Program control flags.
     make_new_mdp = False
+    write_mdp_policy_csv = False
+    gather_new_data = False
+    perform_new_inference = True
+    plot_all_grids = True
+    plot_example_kernel = False
+    # Kernel plot config
+    make_new_mdp = False
+    write_mdp_policy_csv = False
     gather_new_data = False
     perform_new_inference = False
+    plot_all_grids = True
+    plot_example_kernel = False
 
     if make_new_mdp:
-        EM_mdp, VI_mdp, policy_keys_to_print = makeGridMDPxDRA(do_print=True)
+        EM_mdp, VI_mdp, policy_keys_to_print, policy_difference = makeGridMDPxDRA(do_print=True)
         mdp_file = getOutFile()
         with open(mdp_file, 'w+') as _file:
             print "Pickling EM_mdp to {}".format(mdp_file)
-            pickle.dump([EM_mdp, VI_mdp, policy_keys_to_print], _file)
+            pickle.dump([EM_mdp, VI_mdp, policy_keys_to_print,policy_difference], _file)
     else:
         # Manually choose file here:
-        mdp_file = os.path.join(mdp_obj_path, 'EM_MDP_UTC171210_1025')
+        mdp_file = os.path.join(mdp_obj_path, 'EM_MDP_UTC171212_1651')
         print "Loading file {}.".format(mdp_file)
         with open(mdp_file) as _file:
-            EM_mdp, VI_mdp, policy_keys_to_print = pickle.load(_file)
+            EM_mdp, VI_mdp, policy_keys_to_print, policy_difference = pickle.load(_file)
+    if write_mdp_policy_csv:
+        diff_csv_dict = {key: policy_difference[key] for key in policy_keys_to_print}
+        with open(mdp_file+'_Policy_difference.csv', 'w+') as csv_file:
+            writer = csv.writer(csv_file)
+            for key, value in diff_csv_dict.items():
+                for subkey, sub_value in value.items():
+                    writer.writerow([key,subkey,sub_value])
+        #EM_csv_dict = {key: EM_mdp.policy[key] for key in policy_keys_to_print}
+        #with open(mdp_file+'_EM_Policy.csv', 'w+') as csv_file:
+        #    writer = csv.writer(csv_file)
+        #    for key, value in EM_csv_dict.items():
+        #        writer.writerow([key, value])
+        #VI_csv_dict = {key: VI_mdp.policy[key] for key in policy_keys_to_print}
+        #with open(mdp_file+'_VI_policy.csv', 'w+') as csv_file:
+        #    writer = csv.writer(csv_file)
+        #    for key, value in VI_csv_dict.items():
+        #        writer.writerow([key, value])
 
     # Choose which policy to use for demonstration.
     mdp = EM_mdp
@@ -388,7 +424,7 @@ if __name__=='__main__':
         # Use policy to simulate and record results.
         #
         # Current policy E{T|R} 6.7. Start by simulating 10 steps each episode.
-        num_episodes = 400
+        num_episodes = 300
         steps_per_episode = 20
         run_histories = np.zeros([num_episodes, steps_per_episode], dtype=np.int8)
         for episode in range(num_episodes):
@@ -404,12 +440,13 @@ if __name__=='__main__':
             pickle.dump(run_histories, _file)
     else:
         # Manually choose data to load here:
-        history_file = os.path.join(data_path, 'EM_MDP_UTC171209_2149_HIST_200eps10steps_UTC171209_2258')
+        history_file = os.path.join(data_path, 'EM_MDP_UTC171212_1344_HIST_300eps20steps_UTC171212_1505')
         print "Loading history data file {}.".format(history_file)
         with open(history_file) as _file:
             run_histories = pickle.load(_file)
 
     if perform_new_inference:
+        tic = time.clock()
         # Solve for approximated observed policy.
         # Use a new mdp to model created/loaded one and a @ref GridGraph object to record, and seach for shortest paths
         # between two grid-cells.
@@ -421,13 +458,14 @@ if __name__=='__main__':
         # Geodesic Gaussian Kernels, defined as Eq. 3.2 in Statistical Reinforcement
         # Learning, Sugiyama, 2015.
         ggk_sig = 1;
-        kernel_centers = [0, 4, 12, 20, 24]
-        kernel_centers = range(0,25,2)
+        kernel_centers = range(0,grid_map.size,2)
+        print ' Performing inference with kernels at:'
+        pprint(kernel_centers)
         # Note that this needs to be the same instance of `GridGraph` assigned to the MDP!
-        gg_kernel_func = lambda s_i, C_i: np.exp( -graph.shortestPathLength(s_i, C_i)**2 / (2*ggk_sig**2) )
+        infer_mdp.gg_kernel_func = lambda s_i, C_i: np.exp( -graph.shortestPathLength(s_i, C_i)**2 / (2*ggk_sig**2) )
         # Note that we need to use a keyword style argument passing to ensure that
         # each lambda function gets its own value of C.
-        K = [lambda s, C=cent: gg_kernel_func(s, C)
+        K = [lambda s, C=cent: infer_mdp.gg_kernel_func(s, C)
              for cent in kernel_centers]
         # It could be worth pre-computing all of the feature vectors for a small
         # grid...
@@ -436,6 +474,8 @@ if __name__=='__main__':
 
         # Infer the policy from the recorded data.
         infer_mdp.inferPolicy(histories=run_histories, do_print=True, use_precomputed_phi=True)
+        toc = time.clock() -tic
+        print 'Total time to infer policy: {} sec, or {} min.'.format(toc, toc/60.0)
         infered_mdp_file = getOutFile(os.path.basename(history_file) + '_Policy', infered_mdps_path)
         with open(infered_mdp_file, 'w+') as _file:
             print "Pickling Infered Policy to {}.".format(infered_mdp_file)
@@ -443,37 +483,56 @@ if __name__=='__main__':
     else:
         # Manually choose data to load here:
         infered_mdp_file = os.path.join(infered_mdps_path,
-            'EM_MDP_UTC171209_2149_HIST_200eps10steps_UTC171209_2258_Policy_UTC171210_1511')
+            'EM_MDP_UTC171212_1344_HIST_300eps20steps_UTC171212_1505_Policy_UTC171212_1609')
         print "Loading infered policy data file {}.".format(infered_mdp_file)
         with open(infered_mdp_file) as _file:
             infer_mdp = pickle.load(_file)        # Reconsturct Policy with Q(s,a) = <theta, phi(s,a)>
+    if write_mdp_policy_csv:
+        infer_csv_dict = {key: infer_mdp.policy[key] for key in infer_mdp.policy.keys()}
+        with open(infered_mdp_file+'.csv', 'w+') as csv_file:
+            writer = csv.writer(csv_file)
+            for key, value in infer_csv_dict.items():
+                for subkey, sub_value in value.items():
+                    writer.writerow([key,subkey,sub_value])
 
-    # Create plots for comparison.
-    maze = np.zeros(np.array(grid_dim)+1)
-    for state, label in labels.iteritems():
-        if label==red:
-            grid_row, grid_col = np.where(grid_map==int(state))
-            maze[grid_row, grid_col] = 2
-        if label==green:
-            grid_row, grid_col = np.where(grid_map==int(state))
-            maze[grid_row, grid_col] = 1
-    cmap = mcolors.ListedColormap(['w','green','red'])
-    center_offset = 0.5 # Shifts points into center of cell.
-    base_policy_grid = PlotPolicy(maze, cmap, center_offset)
-    plot_policies = [VI_mdp.policy, EM_mdp.policy, infer_mdp.policy]
-    only_use_print_keys = [True, True, False]
-    titles = ['Value Iteration', 'Expecation Maximization', 'Learned']
-    for policy, use_print_keys, title in zip(plot_policies, only_use_print_keys, titles):
-        # Reorder policy dict for plotting.
-        if use_print_keys: # VI and EM policies have DRA states in policy keys.
-            list_of_tuples = [(key, policy[key]) for key in policy_keys_to_print]
-        else: # Learned policy only has state numbers.
-            order_of_keys = [str(key) for key in range(grid_map.size)]
-            list_of_tuples = [(key, policy[key]) for key in order_of_keys]
-        policy = OrderedDict(list_of_tuples)
-        fig = base_policy_grid.confiigurePlot(title, policy, action_list, use_print_keys, policy_keys_to_print,
-                                              decimals=2)
+    if plot_all_grids or plot_example_kernel:
+        # Create plots for comparison.
+        maze = np.zeros(np.array(grid_dim)+1)
+        for state, label in labels.iteritems():
+            if label==red:
+                grid_row, grid_col = np.where(grid_map==int(state))
+                maze[grid_row, grid_col] = 2
+            if label==green:
+                grid_row, grid_col = np.where(grid_map==int(state))
+                maze[grid_row, grid_col] = 1
+        cmap = mcolors.ListedColormap(['w','green','red'])
 
-    print '\n\nHEY! You! With the face! (computers don\'t have faces) Mazimize figure window to correctly show ' \
-            'arrow/dot size ratio!\n'
-    plt.show()
+    if plot_all_grids:
+        center_offset = 0.5 # Shifts points into center of cell.
+        base_policy_grid = PlotPolicy(maze, cmap, center_offset)
+        plot_policies = [VI_mdp.policy, EM_mdp.policy, infer_mdp.policy]
+        only_use_print_keys = [True, True, False]
+        titles = ['Value Iteration', 'Expecation Maximization', 'Learned']
+        for policy, use_print_keys, title in zip(plot_policies, only_use_print_keys, titles):
+            # Reorder policy dict for plotting.
+            if use_print_keys: # VI and EM policies have DRA states in policy keys.
+                list_of_tuples = [(key, policy[key]) for key in policy_keys_to_print]
+            else: # Learned policy only has state numbers.
+                order_of_keys = [str(key) for key in range(grid_map.size)]
+                list_of_tuples = [(key, policy[key]) for key in order_of_keys]
+            policy = OrderedDict(list_of_tuples)
+            fig = base_policy_grid.confiigurePlot(title, policy, action_list, use_print_keys, policy_keys_to_print,
+                                                  decimals=2)
+
+        print '\n\nHEY! You! With the face! (computers don\'t have faces) Mazimize figure window to correctly show ' \
+                'arrow/dot size ratio!\n'
+        plt.show()
+
+    if plot_example_kernel:
+        import pdb; pdb.set_trace()
+        kernel_grid =PlotKernel(maze, cmap)
+        kern_cell = 0
+        act = 'North'
+        title='Kernel Centered at {} for action {}.'.format(kern_cell, act)
+        fig, ax =  kernel_grid.configurePlot(title, kern_cell, act, infer_mdp.kernels)
+        pass
