1f95f0b831f54e8ac9484e32393a7056371a8fcb
Nolan Poulin <nolanpster@gmail.com>
2018-02-21 16:11:03 -0500

    Explicitly initialize kernel std-devs as floats

--- run_mdp_proactive_policy_inference.py
+++ run_mdp_proactive_policy_inference.py
@@ -271,9 +271,9 @@ if __name__=='__main__':
     inference_method='default' # Default chooses gradient ascent. Other options: 'historyMLE', 'iterativeBayes'.
 
     # Gradient Ascent kernel configurations
-    kernel_sigmas = [1]*16
     kernel_count_start = 6
     kernel_count_end = 5
+    kernel_sigmas = np.array([1]*kernel_count_start, dtype=np.float32)
     kernel_count_increment_per_set = -1
     kernel_set_sample_count = 1
     batch_size_for_kernel_set = 1
