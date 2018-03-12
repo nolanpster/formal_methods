4c7404c5b79603406a173240366421590e334cd9
Nolan Poulin <nolanpster@gmail.com>
2018-03-12 18:50:45 -0400

    MultiAgent Test script performs policy inference

--- multi_agent_inference.py
+++ multi_agent_inference.py
@@ -99,22 +99,28 @@ env_action_list = joint_action_list[(env_idx * num_grid_actions) : (env_idx * nu
 ########################################################################################################################
 # MDP solution/load options. If @c make_new_mdp is false load the @c pickled_mdp_file.
 make_new_mdp = False
-pickled_mdp_file_to_load  = 'multi_agent_mdps_180312_1232'
+pickled_mdp_file_to_load  = 'multi_agent_mdps_180312_2300'
 
 
 # Demonstration history set of  episodes (aka trajectories) create/load options. If @c gather_new_data is false,
 # load the @c pickled_episodes_file. If @c gather_new_data is true, use @c num_episodes and @c steps_per_episode to
 # determine how large the demonstration set should be.
 gather_new_data = False
-num_episodes = 250
-steps_per_episode = 15
-pickled_episodes_file_to_load = 'multi_agent_mdps_180312_1232_HIST_250eps15steps_180312_1232'
+print_history_analysis = False
+num_episodes = 1000
+steps_per_episode = 10
+pickled_episodes_file_to_load = 'multi_agent_mdps_180312_1952_HIST_1000eps10steps_180312_2124'
+
+# Perform/load policy inference options. If @c perform_new_inference is false, load the @pickled_inference_mdps_file.
+perform_new_inference = True
+pickled_inference_mdps_file_to_load  = 'robot_mdps_180311_1149_HIST_250eps15steps_180311_1149_Policy_180311_1149'
+inference_method = 'gradientAscent'
 
 # Plotting flags
 plot_all_grids = False
 plot_VI_mdp_grids = False
 plot_EM_mdp_grids = False
-plot_inferred_mdp_grids = False
+plot_inferred_mdp_grids = True
 plot_flags = [plot_all_grids, plot_VI_mdp_grids, plot_EM_mdp_grids, plot_inferred_mdp_grids]
 ########################################################################################################################
 # Create / Load Multi Agent MDP
@@ -134,38 +140,77 @@ if make_new_mdp:
     pickled_mdp_file = DataHelper.pickleMDP(variables_to_save, name_prefix="multi_agent_mdps")
 else:
     (VI_mdp, policy_keys_to_print, pickled_mdp_file) = DataHelper.loadPickledMDP(pickled_mdp_file_to_load)
-    reference_policy_vec = VI_mdp.getPolicyAsVec(policy_keys_to_print)
+
+reference_policy_vec = VI_mdp.getPolicyAsVec(policy_keys_to_print)
 
 ########################################################################################################################
 # Demonstrate Trajectories
 ########################################################################################################################
+demo_mdp = VI_mdp
 if gather_new_data:
     # Use policy to simulate and record results.
     #
     # Current policy E{T|R} 6.7. Start by simulating 10 steps each episode.
-    hist_dtype = DataHelper.getSmallestNumpyUnsignedIntType(VI_mdp.num_observable_states)
+    hist_dtype = DataHelper.getSmallestNumpyUnsignedIntType(demo_mdp.num_observable_states)
     run_histories = np.zeros([num_episodes, steps_per_episode], dtype=hist_dtype)
     for episode in range(num_episodes):
         # Create time-history for this episode.
-        _, run_histories[episode, 0] = VI_mdp.resetState()
+        _, run_histories[episode, 0] = demo_mdp.resetState()
         for t_step in range(1, steps_per_episode):
-            _, run_histories[episode, t_step] = VI_mdp.step()
+            _, run_histories[episode, t_step] = demo_mdp.step()
     pickled_episodes_file = DataHelper.pickleEpisodes(variables_to_save=[run_histories], name_prefix=pickled_mdp_file,
-                                                    num_episodes=num_episodes, steps_per_episode=steps_per_episode)
+                                                      num_episodes=num_episodes, steps_per_episode=steps_per_episode)
 else:
     # Load pickled episodes. Note that trailing comma on assignment automatically unpacks run_histories from a list.
     (run_histories, pickled_episodes_file) = DataHelper.loadPickledEpisodes(pickled_episodes_file_to_load)
     num_episodes = run_histories.shape[0]
     steps_per_episode = run_histories.shape[1]
 
-DataHelper.printHistoryAnalysis(run_histories, VI_mdp.observable_states, labels, empty, robot_goal_states)
-DataHelper.printStateHistories(run_histories, VI_mdp.observable_states)
+if print_history_analysis:
+    DataHelper.printHistoryAnalysis(run_histories, demo_mdp.observable_states, labels, empty, robot_goal_states)
+    DataHelper.printStateHistories(run_histories, demo_mdp.observable_states)
+
+########################################################################################################################
+# Test Policy Inference
+########################################################################################################################
+infer_mdp = demo_mdp.infer_env_mdp
+true_env_policy_vec = infer_mdp.getPolicyAsVec(policy_to_convert=VI_mdp.env_policy[env_idx])
+if perform_new_inference:
+    # Infer the policy from the recorded data.
+
+    # Precompute observed actions for all episodes. Should do this in a "history" class.
+    observation_dtype  = DataHelper.getSmallestNumpyUnsignedIntType(demo_mdp.num_actions)
+    observed_action_indeces = np.empty([num_episodes, steps_per_episode], dtype=observation_dtype)
+    for episode in xrange(num_episodes):
+        for t_step in xrange(1, steps_per_episode):
+            this_state_idx = run_histories[episode, t_step-1]
+            this_state = demo_mdp.observable_states[this_state_idx]
+            next_state_idx = run_histories[episode, t_step]
+            next_state = demo_mdp.observable_states[next_state_idx]
+            observed_action = infer_mdp.graph.getObservedAction(this_state, next_state)
+            observed_action_indeces[episode, t_step] = action_list.index(observed_action)
+
+    theta_vec = infer_mdp.inferPolicy(method=inference_method, histories=run_histories, do_print=True,
+                                     reference_policy_vec=true_env_policy_vec, use_precomputed_phi=True)
+
+########################################################################################################################
+# Print Results' analysis
+########################################################################################################################
+# Remember that variable @ref mdp is used for demonstration.
+
+# Use the InferenceMDP to get the true policy vector since they have matching action keys.
+if len(policy_keys_to_print) == infer_mdp.num_states:
+    infered_policy_L1_norm_error = MDP.getPolicyL1Norm(true_env_policy_vec, infer_mdp.getPolicyAsVec())
+    print('L1-norm between reference and inferred policy: {}.'.format(infered_policy_L1_norm_error))
+else:
+    warnings.warn('Demonstration MDP and inferred MDP do not have the same number of states. Perhaps one was '
+                  'loaded from an old file? Not printing policy difference.')
 
 ########################################################################################################################
 # Plot Results
 ########################################################################################################################
-policy_keys_for_env_poses = {env_cell: [(robot_cell, env_cell) for robot_cell in VI_mdp.grid_cell_vec] for env_cell in
-                             VI_mdp.grid_cell_vec}
+policy_keys_for_env_poses = {env_cell: [(robot_cell, env_cell) for robot_cell in demo_mdp.grid_cell_vec] for env_cell in
+                             demo_mdp.grid_cell_vec}
 
 if any(plot_flags):
 
@@ -186,14 +231,17 @@ if any(plot_flags):
         # Maximum value in maze corresponds to green.
         cmap = mcolors.ListedColormap(['white','green'])
 
-    mdp_list, plot_policies, only_use_print_keys, titles, kernel_locations = \
-        PlotHelper.makePlotGroups(plot_all_grids, plot_VI_mdp_grids, plot_EM_mdp_grids, plot_inferred_mdp_grids, VI_mdp)
+    mdp_list, plot_policies, only_use_print_keys, titles, kernel_locations, action_lists = \
+        PlotHelper.makePlotGroups(plot_all_grids, plot_VI_mdp_grids, plot_EM_mdp_grids, plot_inferred_mdp_grids,
+                                  VI_mdp=VI_mdp, infer_mdp=infer_mdp, robot_action_list=robot_action_list,
+                                  env_action_list=env_action_list)
 
     if plot_all_grids or plot_VI_mdp_grids or plot_EM_mdp_grids or plot_inferred_mdp_grids:
         center_offset = 0.5 # Shifts points into center of cell.
         base_policy_grid = PlotHelper.PlotPolicy(maze, cmap, center_offset)
-        for mdp_to_plot, policy, use_print_keys, title, kernel_loc in zip(mdp_list, plot_policies, only_use_print_keys,
-                                                                          titles, kernel_locations):
+        for mdp_to_plot, policy, use_print_keys, title, kernel_loc, act_list in zip(mdp_list, plot_policies,
+                                                                                    only_use_print_keys, titles,
+                                                                                    kernel_locations, action_lists):
             key_slicer = mdp_to_plot.cell_state_slicer
             # Reorder policy dict for plotting.
             if use_print_keys: # VI and EM policies have DRA states in policy keys.
@@ -206,7 +254,7 @@ if any(plot_flags):
             for env_pose in policy_keys_for_env_poses.keys():
 
                 # Get policy at desired states to plot.
-                list_of_tuples = [(key, policy[(key,)]) for key in policy_keys_for_env_poses[env_pose]]
+                list_of_tuples = [(key, policy[key]) for key in policy_keys_for_env_poses[env_pose]]
                 policy_to_plot = OrderedDict(list_of_tuples)
 
                 # Update the grid colors, assuming the environment is in a fixed locaiton.
@@ -222,9 +270,9 @@ if any(plot_flags):
                             maze[grid_row, grid_col] = 1
                 base_policy_grid.updateCellColors(maze_cells=maze)
 
-                fig = base_policy_grid.configurePlot(title, policy_to_plot, robot_action_list, use_print_keys,
+                fig = base_policy_grid.configurePlot(title, policy_to_plot, act_list, use_print_keys,
                                                      policy_keys_for_env_poses[env_pose], decimals=2,
-                                                     kernel_locations=kernel_loc, stay_action='0_Empty')
+                                                     kernel_locations=kernel_loc, stay_action=act_list[0])
 
         print '\n\nHEY! You! With the face! (computers don\'t have faces) Mazimize figure window to correctly show ' \
                 'arrow/dot size ratio!\n'
