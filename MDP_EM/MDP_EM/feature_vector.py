Update Gradient ascent to use fixed kernels with multiple agents

MDP_EM/MDP_EM/feature_vector.py
@@ -14,7 +14,8 @@ class FeatureVector(object):
     np_all = np.s_[:] # Slice operator for all values.
 
     def __init__(self, action_list, trans_prob_function, graph, ggk_centers=frozenset([]), ogk_centers=frozenset([]),
-                 std_devs=None, dtype=np.float64, ggk_mobile_indices=[], ogk_mobile_indices=[], state_list=[]):
+                 std_devs=None, dtype=np.float64, ggk_mobile_indices=[], ogk_mobile_indices=[], state_list=[],
+                 state_idx_to_infer=0, mobile_kernel_state_idx=None):
         """
         @brief Creates and instance of a feature vector class.
 
@@ -43,6 +44,8 @@ class FeatureVector(object):
         self.trans_prob_func = trans_prob_function
         self.graph = graph
         self.dtype = dtype
+        self.state_idx_to_infer = state_idx_to_infer
+        self.mobile__kernel_state_idx = None
 
         # Prepare Geodesic Gaussian Kernels
         self.ggk_centers = ggk_centers
@@ -111,18 +114,17 @@ class FeatureVector(object):
         if self.has_mobile_kernels:
             self.updateKernelWeights(self.mobile_indices)
 
-        state_idx = self.states.index(state)
         action_idx = self.action_list.index(action)
         phi_mat = np.zeros([self.num_actions, self.num_kernels])
-        phi_mat[action_idx, :] = self.weighted_prob_kernel_sum[:, state_idx, action_idx]
+        phi_mat[action_idx, :] = self.weighted_prob_kernel_sum[:, state[self.state_idx_to_infer], action_idx]
 
         return phi_mat.transpose().ravel().transpose()
 
     def buildTransProbMat(self):
-        self.prob_mat = np.empty([self.num_states, self.num_states, self.num_actions], dtype=self.dtype)
-        for state_idx, state in enumerate(self.states):
+        self.prob_mat = np.empty([self.num_cells, self.num_cells, self.num_actions], dtype=self.dtype)
+        for cell in self.grid_cell_vec:
             for act_idx, action in enumerate(self.action_list):
-                self.prob_mat[state_idx, :, act_idx] = self.trans_prob_func(state, action)
+                self.prob_mat[cell, :, act_idx] = self.trans_prob_func(cell, action)
 
     def updateStdDevs(self, std_devs=None, also_update_kernel_weights=True):
         if std_devs is not None:
@@ -188,6 +190,6 @@ class FeatureVector(object):
         # Below, the prob-mat has dimsion |S|x|S|x|A|, the first axis of states is indexed with 'i'.
         for kernel_idx in selected_kernel_indices:
             for act_idx in xrange(self.num_actions):
-                for state_idx in xrange(self.num_states):
-                    self.weighted_prob_kernel_sum[kernel_idx, state_idx, act_idx] = \
-                        np.inner(self.kernel_values[kernel_idx], self.prob_mat[state_idx, :, act_idx])
+                for cell in self.grid_cell_vec:
+                    self.weighted_prob_kernel_sum[kernel_idx, cell, act_idx] = \
+                        np.inner(self.kernel_values[kernel_idx], self.prob_mat[cell, :, act_idx])

MDP_EM/MDP_EM/multi_agent_mdp.py
@@ -94,15 +94,23 @@ class MultiAgentMDP(MDP):
         self.executable_action_dict.update(
             {unc_agent_idx: [str(unc_agent_idx) + '_' + act for act in self.action_dict[unc_agent_idx]]
              for unc_agent_idx in self.uncontrollable_agent_indices})
-        #### !!! This is the container for the TRUE environmental policy !!! ####
+
+        # Update the grid_probabilility transition matrices to have keys associated with the uncontrolable agent (for
+        # kernel transition functions). (Don't need to copy grid_prob.
+        for new_act_key, old_act_key in zip(self.executable_action_dict[self.uncontrollable_agent_indices[0]],
+                                            self.action_dict[self.uncontrollable_agent_indices[0]]):
+            self.grid_prob[new_act_key] = self.grid_prob.pop(old_act_key)
+
+       #### !!! This is the container for the TRUE environmental policy !!! ####
         self.env_policy = {agent_idx:{} for agent_idx in self.uncontrollable_agent_indices}
         ####                                                                 ####
-        kernel_centers = [frozenset([0, 2, 4, 6, 8])]
-        num_kernels_in_set = len(kernel_centers[0])
+        kernel_centers = [0, 2, 4, 6, 8]
+        num_kernels_in_set = len(kernel_centers)
         kernel_sigmas = np.array([1.]*num_kernels_in_set, dtype=infer_dtype)
+
         self.infer_env_mdp = InferenceMDP(init=self.init,
             action_list=self.executable_action_dict[self.uncontrollable_agent_indices[0]], states=states,
-            prob=deepcopy(self.prob), grid_map=self.grid_map, L=None, gg_kernel_centers=kernel_centers,
+            prob=deepcopy(self.grid_prob), grid_map=self.grid_map, L=None, gg_kernel_centers=kernel_centers,
             kernel_sigmas=kernel_sigmas, state_idx_to_observe=self.uncontrollable_agent_indices[0],
             fixed_obstacle_labels=self.fixed_obstacle_labels)
 
@@ -119,14 +127,14 @@ class MultiAgentMDP(MDP):
 
         @note This is built in a turn-based arena
         """
-        grid_prob = {}
+        self.grid_prob = {}
         if self.neighbor_dict is None:
             self.buildNeighborDict()
         for act in self.action_list:
-            grid_prob[act]=np.zeros((self.num_cells, self.num_cells), self.prob_dtype)
+            self.grid_prob[act]=np.zeros((self.num_cells, self.num_cells), self.prob_dtype)
             for starting_cell in self.grid_cell_vec:
                 for next_cell, act_idx_to_next_state in self.neighbor_dict[starting_cell].iteritems():
-                    grid_prob[act][starting_cell, next_cell] = self.act_prob[act]\
+                    self.grid_prob[act][starting_cell, next_cell] = self.act_prob[act]\
                         [self.act_prob_row_idx_of_grid_cell[starting_cell], act_idx_to_next_state]
 
         self.prob = {}
@@ -147,7 +155,7 @@ class MultiAgentMDP(MDP):
                             else:
                                 # Fill in transition probability for the acting agents motion.
                                 self.prob[joint_act][state_0_idx, state_N_idx] = \
-                                    grid_prob[grid_act][state_0[acting_agent], state_N[acting_agent]]
+                                    self.grid_prob[grid_act][state_0[acting_agent], state_N[acting_agent]]
 
     def buildUniformEnvironmentPolicy(self, agent_idx=None):
         uninform_prob = 1.0/self.num_actions